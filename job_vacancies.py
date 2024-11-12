03cf6ce4626d9cc33147547edf97cdd6d784ee11
Maxim Trigyb <trigub030888@gmail.com>
2024-11-12 22:47:21 +0200

    job_vacancies

--- job_vacancies.py
+++ job_vacancies.py
@@ -1,7 +1,7 @@
 import requests
 import re
 
-# Функція для отримання HTML-контенту
+
 def get_content():
     url = 'https://www.lejobadequat.com/emplois'
     response = requests.get(url)
@@ -9,21 +9,21 @@ def get_content():
     return response.text
 
 
-# Функція для витягування назв вакансій
+
 def extract_job_titles(html):
-    # Регулярний вираз для пошуку назв вакансій
+
     pattern = r'>([A-Za-zÀ-ÿ\s\-/’]+ H/F)<'
     job_titles = re.findall(pattern, html)
     return job_titles
 
-# Основна функція
+
 def main():
     html_content = get_content()
     job_titles = extract_job_titles(html_content)
 
     print(job_titles)
 
-# Виконуємо код
+
 if __name__ == '__main__':
     main()
 
